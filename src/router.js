a912f3e16b7d1c60b802bd0029a78973df82edb2
zerogpm <zerogpm@gmail.com>
2018-10-04 09:50:42 -0700

    final touch

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import store from './store'
 import WelcomePage from './components/welcome/welcome.vue'
 import DashboardPage from './components/dashboard/dashboard.vue'
 import SignupPage from './components/auth/signup.vue'
